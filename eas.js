51acd7cbe776531f05412bd8f49e986876d88835
Lcultist-code <pranav.s7264@gmail.com>
2026-01-08 22:50:00 +0530

    The grids have been coloured and adjusted well enough to be fit in the screen

--- eas.js
+++ eas.js
@@ -7,7 +7,7 @@ const totalCells = gridSize * gridSize;
 
 //creating a grid
 
-for (let i = 0; i <= totalCells; i++) {
+for (let i = 1; i <= totalCells; i++) {
     const cell = document.createElement('div');
     div.appendChild(cell)
     cell.classList.add('flex-item')
