097756351f39a867593c07d6842a33864cc36189
Lcultist-code <pranav.s7264@gmail.com>
2026-01-08 22:39:11 +0530

    eas.css

---
+++ eas.js
@@ -0,0 +1,14 @@
+
+//Acessing main div
+
+const div = document.getElementById('grid-container');
+const gridSize = 16;
+const totalCells = gridSize * gridSize;
+
+//creating a grid
+
+for (let i = 0; i <= totalCells; i++) {
+    const cell = document.createElement('div');
+    div.appendChild(cell)
+    cell.classList.add('flex-item')
+}
